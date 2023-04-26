916f31ae5730799e939a591e49d425d251f0326f
K Santhosh <87223844+Ksanthosh20ka1a0554@users.noreply.github.com>
2023-04-26 23:02:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#faq-1').mouseenter(function(){
+      $("#faq-1-1").slideDown("fast");
+      
+    });
+    $("#faq-1").mouseleave(function(){
+      $("#faq-1-1").slideUp("fast");
+      
+    });
+    $("#faq-2").mouseenter(function(){
+        $("#faq-2-2").slideDown("fast");
+        
+      });
+      $("#faq-2").mouseleave(function(){
+        $("#faq-2-2").slideUp("fast");
+        
+      });
+      $("#faq-3").mouseenter(function(){
+        $("#faq-3-3").slideDown("fast");
+        
+      });
+      $("#faq-3").mouseleave(function(){
+        $("#faq-3-3").slideUp("fast");
+        
+      });
+
+  });
